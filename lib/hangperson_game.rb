bd7590f53a4c010bb74203505f4148217f79a2ef
Vladyslav Kopylash <vladik.kopilash@gmail.com>
2016-09-28 22:09:13 +0300

    implemented game logic

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,16 +7,50 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+
+
+  attr_accessor :word, :wrong_guesses, :guesses
+
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
+
+  def guess(letter)
+    raise ArgumentError if letter.nil? || (letter =~ /[a-z]/i).nil?
+
+    l = letter.downcase
+    valid = true
+    valid = false if @guesses.include?(l) || @wrong_guesses.include?(l)
+
+    if @word.include?(l)
+      @guesses+=l if valid
+    else
+      @wrong_guesses+=l if valid
+    end
+
+    valid
+  end
+
+  def word_with_guesses
+    @word.chars.map { |c| @guesses.include?(c) ? c : '-' }.join('')
+  end
+
+  def check_win_or_lose
+    if @wrong_guesses.length < 7
+      return :win if word_with_guesses == @word
+      return :play
+    end
+    :lose
+  end
+
 
   def self.get_random_word
     require 'uri'
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
-    Net::HTTP.post_form(uri ,{}).body
+    Net::HTTP.post_form(uri, {}).body
   end
 
 end
